49c3783ca33c4d82c772fb6e8c3f6364fde3c7e8
Kylian <kylian.patry@gmail.com>
2025-03-20 11:01:13 +0100

    Adding posts CRUD

--- src/Security/Voter/IsCreatorVoter.php
+++ src/Security/Voter/IsCreatorVoter.php
@@ -5,13 +5,13 @@ namespace App\Security\Voter;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 use Symfony\Component\Security\Core\User\UserInterface;
-use App\Entity\Product;
+use App\Entity\Post;
 
 final class IsCreatorVoter extends Voter
 {
     protected function supports(string $attribute, mixed $subject): bool
     {
-        return $subject instanceof Product;
+        return $subject instanceof Post;
     }
 
     protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
@@ -21,6 +21,6 @@ final class IsCreatorVoter extends Voter
         if (!$user instanceof UserInterface) {
             return false;
         }
-        return $user === $subject->getUser();
+        return $user === $subject->getUser() || in_array('ROLE_ADMIN', $user->getRoles());
     }
 }
